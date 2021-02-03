42be229a5643290e603faa79cacbd36559400dc0
Jack Firth <jackhfirth@gmail.com>
2021-02-02 19:32:28 -0800

    Fix line numbers in output

--- code-snippet.rkt
+++ code-snippet.rkt
@@ -14,10 +14,16 @@
   [code-snippet-start-line (-> code-snippet? exact-positive-integer?)]))
 
 
-(require math/number-theory
-         racket/format
+(require racket/format
+         racket/math
          racket/sequence
          rebellion/base/immutable-string)
+
+
+(module+ test
+  (require (submod "..")
+           rackunit
+           rebellion/private/static-name))
 
 
 ;@----------------------------------------------------------------------------------------------------
@@ -30,11 +36,10 @@
      (define end-line (code-snippet-end-line this))
      (for ([line (in-lines (open-input-string (code-snippet-raw-text this)))]
            [n (in-naturals)])
-       (write-string
-        (~r (+ n (code-snippet-start-line this))
-            #:min-width (digit-count end-line))
-        out)
-       (write-string "  " out)
+       (define line-number-string
+         (~a (+ n (code-snippet-start-line this)) #:min-width (digit-count end-line)))
+       (write-string line-number-string out)
+       (write-string " " out)
        (when (zero? n)
          (write-string (make-string (code-snippet-start-column this) #\space) out))
        (write-string line out)
@@ -47,4 +52,13 @@
 
 
 (define (digit-count n)
-  (add1 (max-dividing-power 10 n)))
+  (add1 (exact-floor (log n 10))))
+
+
+(module+ test
+  (test-case (name-string digit-count)
+    (check-equal? (digit-count 1) 1)
+    (check-equal? (digit-count 123) 3)
+    (check-equal? (digit-count 10000) 5)
+    (check-equal? (digit-count 10001) 5)
+    (check-equal? (digit-count 9999) 4)))
