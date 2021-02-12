4f9ccce538e5f7a04fe569efd226e5000a8e0e0e
Jack Firth <jackhfirth@gmail.com>
2021-02-11 21:22:21 -0800

    Tweak some formatting

--- let-to-define-test.rkt
+++ let-to-define-test.rkt
@@ -1,33 +1,34 @@
 #lang resyntax/testing/refactoring-test
 
+
 require: resyntax/refactoring-rule standard-refactoring-rules
 
 
 test: "single let binding"
---------------------
+------------------------------
 #lang racket/base
 (define (f)
   (let ([x 1])
     1))
-------------------
+------------------------------
 #lang racket/base
 (define (f)
   (define x 1)
   1)
-------------------
+------------------------------
 
 
 test: "multiple let bindings"
----------------------
+------------------------------
 #lang racket/base
 (define (f)
   (let ([x 1]
         [y 1])
     1))
----------------------
+------------------------------
 #lang racket/base
 (define (f)
   (define x 1)
   (define y 1)
   1)
----------------------
+------------------------------
