f47fe41348552b88e749451dc5418d70e7b7ea06
Jack Firth <jackhfirth@gmail.com>
2021-02-13 03:50:02 -0800

    Add syntax identifier set utilities

---
+++ default-recommendations/private/syntax-identifier-sets.rkt
@@ -0,0 +1,45 @@
+#lang racket/base
+
+
+(require racket/contract/base)
+
+
+(provide
+ (contract-out
+  [in-syntax-identifiers (-> syntax? (sequence/c identifier?))]
+  [syntax-identifiers (-> syntax? (set/c identifier? #:cmp 'equal))]
+  [syntax-free-identifiers (-> syntax? immutable-free-id-set?)]
+  [syntax-bound-identifiers (-> syntax? immutable-bound-id-set?)]))
+
+
+(require racket/sequence
+         racket/set
+         racket/stream
+         rebellion/private/guarded-block
+         syntax/id-set)
+
+
+;@----------------------------------------------------------------------------------------------------
+
+
+(define (in-syntax-identifiers stx)
+  (stream*
+   (guarded-block
+     (guard (identifier? stx) then
+       (stream stx))
+     (guard-match (or (cons head tail) (? syntax? (app syntax-e (cons head tail)))) stx else
+       (stream))
+     (stream-append (in-syntax-identifiers head) (in-syntax-identifiers tail)))))
+
+
+(define (syntax-identifiers stx)
+  (for/set ([id (in-syntax-identifiers stx)])
+    id))
+
+
+(define (syntax-free-identifiers stx)
+  (immutable-free-id-set (syntax-identifiers stx)))
+
+
+(define (syntax-bound-identifiers stx)
+  (immutable-bound-id-set (syntax-identifiers stx)))
