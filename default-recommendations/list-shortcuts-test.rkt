9103c46f60e76e9493432c97ee86fad5cfcfbfae
Jack Firth <jackhfirth@gmail.com>
2021-03-07 22:01:30 -0800

    Add some list shortcuts

---
+++ default-recommendations/list-shortcuts-test.rkt
@@ -0,0 +1,113 @@
+#lang resyntax/testing/refactoring-test
+
+
+require: resyntax/default-recommendations list-shortcuts
+
+
+test: "car reverse of list not refactorable to last of list, due to imports (see issue #11)"
+------------------------------
+#lang racket/base
+(car (reverse (list 1 2 3)))
+------------------------------
+
+
+test: "first reverse of list refactorable to last of list"
+------------------------------
+#lang racket/base
+(require racket/list)
+(first (reverse (list 1 2 3)))
+------------------------------
+#lang racket/base
+(require racket/list)
+(last (list 1 2 3))
+------------------------------
+
+
+test: "list eq? to quoted empty list refactorable to null? check"
+------------------------------
+#lang racket/base
+(eq? (list 1 2 3) '())
+------------------------------
+#lang racket/base
+(null? (list 1 2 3))
+------------------------------
+
+
+test: "list eqv? to quoted empty list refactorable to null? check"
+------------------------------
+#lang racket/base
+(eqv? (list 1 2 3) '())
+------------------------------
+#lang racket/base
+(null? (list 1 2 3))
+------------------------------
+
+
+test: "list equal? to quoted empty list refactorable to null? check"
+------------------------------
+#lang racket/base
+(equal? (list 1 2 3) '())
+------------------------------
+#lang racket/base
+(null? (list 1 2 3))
+------------------------------
+
+
+test: "list eq? to (list) refactorable to null? check"
+------------------------------
+#lang racket/base
+(eq? (list 1 2 3) (list))
+------------------------------
+#lang racket/base
+(null? (list 1 2 3))
+------------------------------
+
+
+test: "list eqv? to (list) refactorable to null? check"
+------------------------------
+#lang racket/base
+(eqv? (list 1 2 3) (list))
+------------------------------
+#lang racket/base
+(null? (list 1 2 3))
+------------------------------
+
+
+test: "list equal? to (list) refactorable to null? check"
+------------------------------
+#lang racket/base
+(equal? (list 1 2 3) (list))
+------------------------------
+#lang racket/base
+(null? (list 1 2 3))
+------------------------------
+
+
+test: "list eq? to null refactorable to null? check"
+------------------------------
+#lang racket/base
+(eq? (list 1 2 3) null)
+------------------------------
+#lang racket/base
+(null? (list 1 2 3))
+------------------------------
+
+
+test: "list eqv? to null refactorable to null? check"
+------------------------------
+#lang racket/base
+(eqv? (list 1 2 3) null)
+------------------------------
+#lang racket/base
+(null? (list 1 2 3))
+------------------------------
+
+
+test: "list equal? to null refactorable to null? check"
+------------------------------
+#lang racket/base
+(equal? (list 1 2 3) null)
+------------------------------
+#lang racket/base
+(null? (list 1 2 3))
+------------------------------
---
+++ default-recommendations/list-shortcuts.rkt
@@ -0,0 +1,47 @@
+#lang racket/base
+
+
+(require racket/contract/base)
+
+
+(provide
+ (contract-out
+  [list-shortcuts refactoring-suite?]))
+
+
+(require (for-syntax racket/base)
+         racket/list
+         rebellion/private/static-name
+         resyntax/refactoring-rule
+         resyntax/refactoring-suite
+         syntax/parse)
+
+
+;@----------------------------------------------------------------------------------------------------
+
+
+(define-refactoring-rule first-reverse-to-last
+  #:description "The last function can be used to get the last item from a list."
+  #:literals (first reverse)
+  ;; This can't match (car (reverse list)) because of https://github.com/jackfirth/resyntax/issues/11
+  [(first (reverse list))
+   (last list)])
+
+
+(define-syntax-class null-test
+  #:attributes (subject)
+  #:literals (eq? eqv? equal? null list quote)
+  (pattern ((~or eq? eqv? equal?) subject (~or null (list) '())))
+  (pattern ((~or eq? eqv? equal?) (~or null (list) '()) subject)))
+
+
+;; This can't suggest using empty? because of https://github.com/jackfirth/resyntax/issues/11
+(define-refactoring-rule equal-null-list-to-null-predicate
+  #:description "The null? predicate can be used to test for the empty list."
+  [test:null-test (null? test.subject)])
+
+
+(define list-shortcuts
+  (refactoring-suite
+   #:name (name list-shortcuts)
+   #:rules (list equal-null-list-to-null-predicate first-reverse-to-last)))
