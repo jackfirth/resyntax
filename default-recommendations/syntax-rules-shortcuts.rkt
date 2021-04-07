8ef9604466a22e2b2a62e9515e127670b7259eb8
Jack Firth <jackhfirth@gmail.com>
2021-04-07 00:12:43 -0700

    Add shortcuts for syntax-rules
    Closes #99.

---
+++ default-recommendations/syntax-rules-shortcuts.rkt
@@ -0,0 +1,35 @@
+#lang racket/base
+
+
+(require racket/contract/base)
+
+
+(provide
+ (contract-out
+  [syntax-rules-shortcuts refactoring-suite?]))
+
+
+(require rebellion/private/static-name
+         resyntax/refactoring-rule
+         resyntax/refactoring-suite
+         resyntax/syntax-replacement
+         syntax/parse)
+
+
+;@----------------------------------------------------------------------------------------------------
+
+
+(define-refactoring-rule define-syntax-syntax-rules-to-define-syntax-rule
+  #:description "This macro can be replaced with a simpler, equivalent define-syntax-rule macro."
+  #:literals (define-syntax syntax-rules)
+  [(define-syntax macro:id
+     (syntax-rules () [(_ . pattern) template]))
+   (define-syntax-rule (macro . pattern)
+     NEWLINE template)])
+
+
+(define syntax-rules-shortcuts
+  (refactoring-suite
+   #:name (name syntax-rules-shortcuts)
+   #:rules
+   (list define-syntax-syntax-rules-to-define-syntax-rule)))
