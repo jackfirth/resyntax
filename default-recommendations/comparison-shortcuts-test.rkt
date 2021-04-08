099e66ccd23adc8485ea3defc69bc076ca4be62a
Jack Firth <jackhfirth@gmail.com>
2021-04-07 17:27:21 -0700

    Allow specifying multiple inputs in refactoring tests
    The test checks that *each input* produces the expected output (the last code block/line is always the expected output). This makes it easier to write tests for conditionals and comparisons, since those test cases tend to have a combinatoric explosion in the number of ways that the same code can be written.

--- default-recommendations/comparison-shortcuts-test.rkt
+++ default-recommendations/comparison-shortcuts-test.rkt
@@ -12,41 +12,27 @@ header:
 ------------------------------
 
 
-test: "(> (- x y) 0) refactorable to direct comparison"
+test: "comparison of difference to zero refactorable to direct > comparison"
 - (> (- x y) 0)
+- (<= 0 (- x y))
 - (> x y)
 
 
-test: "(< (- x y) 0) refactorable to direct comparison"
+test: "comparison of difference to zero refactorable to direct < comparison"
 - (< (- x y) 0)
-- (< x y)
-
-
-test: "(>= (- x y) 0) refactorable to direct comparison"
-- (>= (- x y) 0)
-- (>= x y)
-
-
-test: "(<= (- x y) 0) refactorable to direct comparison"
-- (<= (- x y) 0)
-- (<= x y)
-
-
-test: "(> 0 (- x y)) refactorable to direct comparison"
-- (> 0 (- x y))
-- (<= x y)
-
-
-test: "(< 0 (- x y)) refactorable to direct comparison"
-- (< 0 (- x y))
-- (>= x y)
-
-
-test: "(>= 0 (- x y)) refactorable to direct comparison"
 - (>= 0 (- x y))
 - (< x y)
 
 
-test: "(<= 0 (- x y)) refactorable to direct comparison"
-- (<= 0 (- x y))
-- (> x y)
+test: "comparison of difference to zero refactorable to direct >= comparison"
+- (< 0 (- x y))
+- (>= (- x y) 0)
+- (>= x y)
+
+
+test: "comparison of difference to zero refactorable to direct <= comparison"
+- (> 0 (- x y))
+- (<= (- x y) 0)
+- (<= x y)
+
+
