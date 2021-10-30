5f557e4002db866db982ffeb25ee2330316a15b1
Jack Firth <jackhfirth@gmail.com>
2021-10-29 23:28:29 -0700

    Fix up tests
    Accidentally overlooked some copy-paste errors.

--- default-recommendations/hash-shortcuts-test.rkt
+++ default-recommendations/hash-shortcuts-test.rkt
@@ -47,21 +47,6 @@ test: "hash-ref with hash-set! lambda with constant can be simplified to hash-re
 ------------------------------
 
 
-test: "hash-ref with hash-set! lambda with constant can be simplified to hash-ref!"
-------------------------------
-(define h (make-hash))
-(define k 'a)
-(hash-ref h k (λ ()
-                (hash-set! h k 0)
-                0))
-------------------------------
-------------------------------
-(define h (make-hash))
-(define k 'a)
-(hash-ref! h k 0)
-------------------------------
-
-
 test: "hash-ref with hash-set! lambda with thunk can be simplified to hash-ref!"
 ------------------------------
 (define h (make-hash))
@@ -75,23 +60,4 @@ test: "hash-ref with hash-set! lambda with thunk can be simplified to hash-ref!"
 (define h (make-hash))
 (define k 'a)
 (hash-ref! h k make-hash)
-------------------------------
-
-
-test: "hash-ref with hash-set! lambda with thunk can be simplified to hash-ref!"
-------------------------------
-(require racket/set)
-(define student-grades (make-hash))
-(define student "kevin")
-(hash-ref student-grades student
-          (λ ()
-            (define grades (mutable-set))
-            (hash-set! student-grades student grades)
-            grades))
-------------------------------
-------------------------------
-(require racket/set)
-(define student-grades (make-hash))
-(define student "kevin")
-(hash-ref! student-grades student mutable-set)
 ------------------------------
