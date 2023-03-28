37516ba3ef60bf84a56552377e5a634546120f57
Jack Firth <jackhfirth@gmail.com>
2023-03-27 23:30:15 -0700

    Suggest splitting `define-values` when appropriate
    Closes #205.

---
+++ default-recommendations/definition-shortcuts-test.rkt
@@ -0,0 +1,24 @@
+#lang resyntax/testing/refactoring-test
+
+
+require: resyntax/default-recommendations definition-shortcuts
+
+
+header:
+- #lang racket/base
+
+
+test: "define-values with values refactorable to separate definitions"
+------------------------------
+(define (foo)
+  (define-values (a b c)
+    (values 1 2 3))
+  (+ a b c))
+------------------------------
+------------------------------
+(define (foo)
+  (define a 1)
+  (define b 2)
+  (define c 3)
+  (+ a b c))
+------------------------------
