fe391df7d05c37a6b1541b9cf1e352736d31fe21
Jack Firth <jackhfirth@gmail.com>
2024-09-01 19:21:57 -0700

    Add `define-definition-context-refactoring-rule`
    This makes it a lot easier to write more rules that target definition contexts.

--- refactoring-rule.rkt
+++ refactoring-rule.rkt
@@ -6,6 +6,7 @@
 
 (provide
  define-refactoring-rule
+ define-definition-context-refactoring-rule
  (contract-out
   [refactoring-rule? predicate/c]
   [refactoring-rule-description (-> refactoring-rule? immutable-string?)]
@@ -20,10 +21,12 @@
          (option/c syntax-replacement?))])))
 
 
-(require (for-syntax racket/base)
+(require (for-syntax racket/base
+                     racket/syntax)
          rebellion/base/immutable-string
          rebellion/base/option
          rebellion/type/object
+         resyntax/default-recommendations/private/definition-context
          resyntax/private/source
          resyntax/private/syntax-replacement
          resyntax/private/syntax-neighbors
@@ -62,7 +65,7 @@
       #:introduction-scope rule-introduction-scope))))
 
 
-(define-simple-macro
+(define-syntax-parse-rule
   (define-refactoring-rule id:id
     #:description description
     parse-option ...
@@ -79,3 +82,48 @@
            parse-option ...
            [pattern pattern-directive ... (present #'replacement)]
            [_ absent]))))))
+
+
+(define-syntax-parse-rule
+  (define-definition-context-refactoring-rule id:id
+    #:description (~var description (expr/c #'string?))
+    parse-option ...
+    [splicing-pattern pattern-directive ... (splicing-replacement ...)])
+
+  ;; These identifiers are macro-introduced, but we use format-id on them anyway so that the expanded
+  ;; code is more readable and it's clearer which refactoring rule these syntax classes are derived
+  ;; from.
+  #:with body-matching-id (format-id #'macro-introduced-context "body-matching-~a" #'id)
+  #:with expression-matching-id (format-id #'macro-introduced-context "expression-matching-~a" #'id)
+
+  (begin
+
+    (define-splicing-syntax-class body-matching-id
+      #:attributes ([refactored 1])
+      parse-option ...
+      (pattern splicing-pattern
+        pattern-directive ...
+        #:with (refactored (... ...)) #'(splicing-replacement ...)))
+
+    (define-syntax-class expression-matching-id
+      #:attributes (refactored)
+
+      (pattern (header:header-form-allowing-internal-definitions (~var body body-matching-id))
+        #:cut
+        #:with refactored #'(header.original (... ...) body.refactored (... ...)))
+
+      (pattern (branching-header:branching-form-allowing-internal-definitions-within-clauses
+                clause-before (... ...)
+                (~and original-clause [clause-header (~var body body-matching-id)])
+                clause-after (... ...))
+        #:cut
+        #:with refactored
+        #'(branching-header.original
+           (... ...)
+           clause-before (... ...)
+           (~replacement [clause-header body.refactored (... ...)] #:original original-clause)
+           clause-after (... ...))))
+
+    (define-refactoring-rule id
+      #:description description
+      [(~var expression expression-matching-id) expression.refactored])))
