966d241451ea56987aeada71caf2ef6cae9f66cd
Jack Firth <jackhfirth@gmail.com>
2024-09-01 20:40:13 -0700

    Add `inline-unnecessary-define` rule
    Closes #158.

--- default-recommendations/definition-shortcuts.rkt
+++ default-recommendations/definition-shortcuts.rkt
@@ -30,8 +30,17 @@
    (body-before ... (define id expr) ... body-after ...)])
 
 
+(define-definition-context-refactoring-rule inline-unnecessary-define
+  #:description "This variable is returned immediately and can be inlined."
+  #:literals (define)
+  [(~seq body-before ... (define id1:id expr) id2)
+   #:when (free-identifier=? #'id1 #'id2)
+   (body-before ... expr)])
+
+
 (define definition-shortcuts
   (refactoring-suite
    #:name (name definition-shortcuts)
    #:rules
-   (list define-values-values-to-define)))
+   (list define-values-values-to-define
+         inline-unnecessary-define)))
