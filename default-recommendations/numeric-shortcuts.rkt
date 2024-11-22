696e9bad3cf4fc4a97c6d4fa9cc6a7c6e3dad8e8
Jacqueline Firth <jackhfirth@gmail.com>
2024-11-22 04:48:01 -0800

    Expand numeric comparison shortcuts (#416)

--- default-recommendations/numeric-shortcuts.rkt
+++ default-recommendations/numeric-shortcuts.rkt
@@ -34,18 +34,46 @@
   sub1)
 
 
+(define-refactoring-rule zero-comparison-to-positive?
+  #:description "This expression is equivalent to calling the `positive?` predicate."
+  #:literals (< > <= >= not)
+  (~or (> e:expr 0)
+       (< 0 e:expr)
+       (not (<= e:expr 0))
+       (not (>= 0 e:expr)))
+  (positive? e))
+
+
 (define-refactoring-rule zero-comparison-lambda-to-positive?
   #:description "This lambda function is equivalent to the built-in `positive?` predicate."
-  #:literals (< >)
-  (lambda:lambda-by-any-name (x1:id) (~or (> x2:id 0) (< 0 x2:id)))
+  #:literals (< > <= >= not)
+  (lambda:lambda-by-any-name (x1:id)
+                             (~or (> x2:id 0)
+                                  (< 0 x2:id)
+                                  (not (<= x2:id 0))
+                                  (not (>= 0 x2:id))))
   #:when (free-identifier=? #'x1 #'x2)
   positive?)
 
 
+(define-refactoring-rule zero-comparison-to-negative?
+  #:description "This expression is equivalent to calling the `negative?` predicate."
+  #:literals (< > <= >= not)
+  (~or (< e:expr 0)
+       (> 0 e:expr)
+       (not (>= e:expr 0))
+       (not (<= 0 e:expr)))
+  (negative? e))
+
+
 (define-refactoring-rule zero-comparison-lambda-to-negative?
   #:description "This lambda function is equivalent to the built-in `negative?` predicate."
-  #:literals (< >)
-  (lambda:lambda-by-any-name (x1:id) (~or (< x2:id 0) (> 0 x2:id)))
+  #:literals (< > <= >= not)
+  (lambda:lambda-by-any-name (x1:id)
+                             (~or (< x2:id 0)
+                                  (> 0 x2:id)
+                                  (not (>= x2:id 0))
+                                  (not (<= 0 x2:id))))
   #:when (free-identifier=? #'x1 #'x2)
   negative?)
 
@@ -70,4 +98,6 @@
            single-argument-plus-to-identity
            sub1-lambda-to-sub1
            zero-comparison-lambda-to-negative?
-           zero-comparison-lambda-to-positive?))
+           zero-comparison-lambda-to-positive?
+           zero-comparison-to-negative?
+           zero-comparison-to-positive?))
