9d5d1815150f457e55909f96f33483eb51bf19ac
Jacqueline Firth <jackhfirth@gmail.com>
2025-04-14 19:09:24 -0700

    Fix a bug in comparison shortcuts (#446)
    See [this comment](https://github.com/racket/drracket/pull/734/commits/5c7c19f6f48511d71279eaef388b365550f9b05c#r2040841364).

--- default-recommendations/comparison-shortcuts-test.rkt
+++ default-recommendations/comparison-shortcuts-test.rkt
@@ -14,24 +14,24 @@ header:
 
 test: "comparison of difference to zero refactorable to direct > comparison"
 - (> (- x y) 0)
-- (<= 0 (- x y))
+- (< 0 (- x y))
 - (> x y)
 
 
 test: "comparison of difference to zero refactorable to direct < comparison"
 - (< (- x y) 0)
-- (>= 0 (- x y))
+- (> 0 (- x y))
 - (< x y)
 
 
 test: "comparison of difference to zero refactorable to direct >= comparison"
-- (< 0 (- x y))
+- (<= 0 (- x y))
 - (>= (- x y) 0)
 - (>= x y)
 
 
 test: "comparison of difference to zero refactorable to direct <= comparison"
-- (> 0 (- x y))
+- (>= 0 (- x y))
 - (<= (- x y) 0)
 - (<= x y)
 
