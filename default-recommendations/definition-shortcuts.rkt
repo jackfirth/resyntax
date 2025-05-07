9b354d62a1cf0c1586da191c99ba1fb4abe6c03f
Jacqueline Firth <jackhfirth@gmail.com>
2025-05-06 20:32:46 -0700

    Fix comment handling `inline-unnecessary-begin` (#455)

--- default-recommendations/definition-shortcuts.rkt
+++ default-recommendations/definition-shortcuts.rkt
@@ -89,9 +89,42 @@
   #:description "This `begin` form can be flattened into the surrounding definition context."
   #:literals (begin)
   (~seq body-before ... (~and original (begin inner-body ...)) body-after ...)
+
   #:with (replacement ...)
-  #'(~focus-replacement-on (~splicing-replacement (inner-body ...) #:original original))
-  (body-before ... inner-body ... body-after ...))
+  (if (and (empty? (attribute body-before)) (empty? (attribute body-after)))
+
+      ; If the begin form being inlined is the *only* form in the surrounding definition context,
+      ; then it's possible the user has formatted it in such a way that replacing it with multiple
+      ; forms could require moving those forms onto surrounding lines. In that case, we explicitly
+      ; don't want to focus the replacement on just the begin forms, because we *should* try to
+      ; reformat the entire surrounding context. For example, refactoring this code:
+      ;
+      ; (map (λ (x) (begin (f x) (g x))) xs)
+      ;
+      ; Should produce this code:
+      ;
+      ; (map (λ (x)
+      ;        (f x)
+      ;        (g x))
+      ;      xs)
+      ;
+      ; And *not* this code:
+      ;
+      ; (map (λ (x) (f x)
+      ;             (g x))
+      ;      xs)
+      ;
+      ; To do that, we have to ensure that the whole lambda form (the entire surrounding definition
+      ; context) is formatted because we're changing that context from a context with one body form
+      ; to a context with potentially multiple body forms.
+      #'(inner-body ...)
+
+      ; In the case that we already know the surrounding context contains other body forms, we know
+      ; we won't need to reformat the whole context. We focus the replacement on just the inline forms
+      ; so that we can ignore things like comments surrounding the inlined (begin ...) form.
+      #'(~focus-replacement-on (~splicing-replacement (inner-body ...) #:original original)))
+
+  (body-before ... replacement ... body-after ...))
 
 
 (define-refactoring-suite definition-shortcuts
