9f6f25c53486abbd9f65ef9b0094f675469fba41
Jack Firth <jackhfirth@gmail.com>
2025-05-20 23:03:33 -0700

    Remove non-lambda `positive?` and `negative?` rules
    These just aren't worth it.

--- default-recommendations/numeric-shortcuts-test.rkt
+++ default-recommendations/numeric-shortcuts-test.rkt
@@ -21,22 +21,6 @@ test: "lambda equivalent to sub1 refactorable to sub1"
 - (map sub1 (list 1 2 3))
 
 
-test: "comparison equivalent to positive? refactorable to positive?"
-- (> 42 0)
-- (< 0 42)
-- (not (<= 42 0))
-- (not (>= 0 42))
-- (positive? 42)
-
-
-test: "comparison equivalent to negative? refactorable to negative?"
-- (< 42 0)
-- (> 0 42)
-- (not (>= 42 0))
-- (not (<= 0 42))
-- (negative? 42)
-
-
 test: "lambda equivalent to positive? refactorable to positive?"
 - (filter (λ (x) (> x 0)) (list -2 -1 0 1 2))
 - (filter (λ (x) (< 0 x)) (list -2 -1 0 1 2))
