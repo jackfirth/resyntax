0b1d5a84d958aa50bf47f6754e31481125b37f5d
Jack Firth <jackhfirth@gmail.com>
2025-10-25 01:45:01 -0700

    Simplify `error-to-raise-arguments-error`

--- default-recommendations/exception-suggestions.rkt
+++ default-recommendations/exception-suggestions.rkt
@@ -36,34 +36,25 @@
 
 (define-refactoring-rule error-to-raise-arguments-error
   #:description
-  "Use `raise-arguments-error` instead of `error` for better error messages that follow Racket conventions."
+  "Use `raise-arguments-error` instead of `error` for better error messages that follow Racket \
+conventions."
   #:literals (error)
   
-  (error sym:expr message:str arg:id ...)
-  #:when (string-contains? (syntax-e #'message) "~a")
-  #:do [(define message-str (syntax-e #'message))
-        (define args-list (syntax->list #'(arg ...)))
-        ;; Count the number of ~a placeholders in the message
-        (define placeholder-count (length (regexp-match* #rx"~a" message-str)))
-        ;; Only proceed if the number of placeholders matches the number of arguments
-        (define proceed? (= placeholder-count (length args-list)))]
-  #:when proceed?
+  (error sym:expr message:str arg:id ...+)
+
+  #:do [(define message-str (syntax-e (attribute message)))
+        (define args-list (attribute arg))]
+  #:when (= (length (regexp-match* #rx"~a" message-str)) (length args-list))
   #:do [(define cleaned-message (string-replace message-str "~a" ""))
         ;; Clean up extra spaces and trailing punctuation from placeholder removal
         (define cleaned-message-normalized
-          (regexp-replace* #rx"  +" cleaned-message " "))
-        (define cleaned-final
-          (regexp-replace #rx"[,;: ]+$" cleaned-message-normalized ""))
-        (define field-pairs
-          (apply append
-                 (map (λ (arg-stx)
-                        (list (datum->syntax #'message (symbol->string (syntax-e arg-stx)))
-                              arg-stx))
-                      args-list)))]
-  #:with new-message cleaned-final
-  #:with (field-pair ...) field-pairs
+          (regexp-replace* #rx"  +" cleaned-message " "))]
+  #:with new-message (regexp-replace #rx"[,;: ]+$" cleaned-message-normalized "")
+  #:with (arg-str ...)
+  (for/list ([arg-id (in-list args-list)])
+    (symbol->string (syntax-e arg-id)))
   
-  (raise-arguments-error sym new-message field-pair ...))
+  (raise-arguments-error sym new-message (~@ arg-str arg) ...))
 
 
 (define-refactoring-suite exception-suggestions
