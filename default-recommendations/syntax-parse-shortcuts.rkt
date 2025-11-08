5f13e170077d809ee306ae9ae0f737a3ac1ae837
Jack Firth <jackhfirth@gmail.com>
2025-11-07 22:09:28 -0800

    Fix implementation with binding check bypass

--- default-recommendations/syntax-parse-shortcuts.rkt
+++ default-recommendations/syntax-parse-shortcuts.rkt
@@ -9,8 +9,11 @@
   [syntax-parse-shortcuts refactoring-suite?]))
 
 
-(require rebellion/private/static-name
+(require (for-syntax racket/base
+                     syntax/parse)
+         rebellion/private/static-name
          resyntax/base
+         resyntax/private/more-syntax-parse-classes
          resyntax/private/syntax-traversal
          racket/stream
          syntax/parse
@@ -24,48 +27,22 @@
   #:description
   "This `define-syntax` macro with a single `syntax-parse` clause can be replaced with a simpler,
 equivalent `define-syntax-parse-rule` macro."
-  #:literals (define-syntax lambda)
+  #:literals (define-syntax lambda [syntax-parse syntax-parse #:phase 1] [syntax-id syntax #:phase 1])
   
   (define-syntax macro:id
-    (lambda (stx-var:id)
-      (syntax-parse-id:id parsed-stx:id
-        [(_ . pattern) body ...])))
+    (lambda (stx-id:id)
+      (syntax-parse stx-id2:id
+        [(_ pattern ...) directive:syntax-parse-pattern-directive ... (syntax-id last-form)])))
   
-  #:do [(define stx-id (attribute stx-var))
-        ;; Check if stx-var is referenced in the body using syntax-search
-        (define body-has-stx-ref?
-          (for/or ([body-part (in-list (attribute body))])
-            (not (stream-empty? 
-                  (syntax-search body-part
-                    [id:id #:when (free-identifier=? #'id stx-id)
-                     (stream this-syntax)])))))
-        ;; Replace references to stx-var with this-syntax and strip syntax wrapper
-        (define (replace-stx-with-this-syntax stx)
-          (syntax-parse stx
-            [id:id #:when (free-identifier=? #'id stx-id)
-             (datum->syntax #'here 'this-syntax stx)]
-            [(a . b)
-             (datum->syntax stx
-                            (cons (replace-stx-with-this-syntax #'a)
-                                  (replace-stx-with-this-syntax #'b))
-                            stx
-                            stx)]
-            [other #'other]))
-        (define (strip-syntax-wrapper stx)
-          (syntax-parse stx
-            #:literals (syntax)
-            [(syntax body) #'body]
-            [other #'other]))
-        (define new-body
-          (for/list ([body-part (in-list (attribute body))])
-            (strip-syntax-wrapper (replace-stx-with-this-syntax body-part))))]
+  #:when (free-identifier=? (attribute stx-id) (attribute stx-id2) 1)
+
+  #:with (new-body ...)
+  (syntax-traverse #'((~@ . directive) ... last-form)
+    [id-in-body:id
+     #:when (free-identifier=? (attribute id-in-body) (attribute stx-id) 1)
+     (syntax-property #'this-syntax 'skip-incorrect-binding-check? #true)])
   
-  #:with (new-body-part ...) new-body
-  
-  #:when (and (free-identifier=? (attribute stx-var) (attribute parsed-stx))
-              (equal? (syntax-e (attribute syntax-parse-id)) 'syntax-parse))
-  
-  (define-syntax-parse-rule (macro . pattern) new-body-part ...))
+  (define-syntax-parse-rule (macro pattern ...) new-body ...))
 
 
 (define-refactoring-rule define-syntax-parser-to-define-syntax-parse-rule-simple
